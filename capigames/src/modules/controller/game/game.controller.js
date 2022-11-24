6b387ec0185a663299e7b3b117abb9bd72e3161d
JoksanBahena <84723100+JoksanBahena@users.noreply.github.com>
2022-11-24 16:47:49 -0600

    act 24/11

--- capigames/src/modules/controller/game/game.controller.js
+++ capigames/src/modules/controller/game/game.controller.js
@@ -1,6 +1,6 @@
 const { Response, Router } = require('express');
 const { validateError } = require ("../../../utils/functions");
-const { findAll } = require("./game.gateway");
+const { findAll, findById, save, update, remove } = require("./game.gateway");
 
 const getAll = async (req, res = Response) => {
     try {
@@ -14,9 +14,76 @@ const getAll = async (req, res = Response) => {
     }
 };
 
+const getById = async (req, res = Response) => {
+    try {
+        const { id } = req.params;
+        const game = await findById(id);
+        res.status(200).json(game);
+
+    }catch(error) {
+        console.log(error);
+        const message = validateError(error);
+        res.status(400).json({ message });
+    }
+};
+
+const insert = async (req, res = Response) => {
+    try {
+      const { name, genre, price, image, description, stock } = req.body;
+      const game = await save({
+        name,
+        genre,
+        price,
+        image,
+        description,
+        stock
+      });
+      res.status(200).json(game);
+    } catch (error) {
+      console.log(error);
+      const message = validateError(error);
+      res.status(400).json({ message });
+    }
+};
+
+const updateGame = async (req, res = Response) => {
+    try {
+        const { name, genre, price, image, description, stock } = req.body;
+        const game = await update({
+            name,
+            genre,
+            price,
+            image,
+            description,
+            stock
+        });
+        res.status(200).json(game);
+    } catch (error) {
+        console.log(error);
+        const message = validateError(error);
+        res.status(400).json({ message });
+    }
+};
+
+const deleteGame = async (req, res = Response) => {
+    try {
+        const { id } = req.params;
+        const game = await remove(id);
+        res.status(200).json(game);
+    } catch (error) {
+        console.log(error);
+        const message = validateError(error);
+        res.status(400).json({ message });
+    }
+};
+
 const gamesRouter = Router();
 
 gamesRouter.get("/", [], getAll);
+gamesRouter.get("/:id", [], getById);
+gamesRouter.post("/", [], insert);
+gamesRouter.put("/", [], updateGame); 
+gamesRouter.delete("/:id", [], deleteGame);
 
 module.exports = {
     gamesRouter,
--- capigames/src/utils/mysql.js
+++ capigames/src/utils/mysql.js
@@ -23,6 +23,27 @@ const query = ( sql, params ) => {
   });
 };
 
+/*
+const query = (sql, values) => {
+  return new Promise((resolve, reject) => {
+    client.getConnection((err, connection) => {
+      if (err) {
+        reject(err);
+      } else {
+        connection.query(sql, values, (err, rows) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(rows);
+          }
+          connection.release();
+        });
+      }
+    });
+  });
+};
+*/
+
 module.exports = {
   query
 };
